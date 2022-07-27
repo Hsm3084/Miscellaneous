399cfce74c0e19a802024f87ec40804ef8c1a31e
Hsm3084 <94923387+Hsm3084@users.noreply.github.com>
2022-07-27 10:24:18 -0500

    Create celsiustofahren.java

---
+++ celsiustofahren.java
@@ -0,0 +1,12 @@
+Public Class Farenheit
+{
+public static void main (String args[])
+{ float Farenheit, Celsius 
+double Farenheit=Farenheit;
+Scanner scan= new Scanner(System.in);.
+System.out.print("Enter first number- ");
+double Farenheit= sc.nextInt();
+double Farenheit=Celsius*(9/5)+32
+System.out.println(Farenheit is: "+Farenheit);
+}
+}
