c3cdc94eb5b7fb1af6dea216dece3d5fa8b38af0
Hsm3084 <94923387+Hsm3084@users.noreply.github.com>
2022-07-27 10:29:01 -0500

    Create FahrentoCelsuis.java

---
+++ FahrentoCelsuis.java
@@ -0,0 +1,12 @@
+Public Class Celsius
+{
+public static void main (String args[])
+{ float Farenheit, Celsius 
+double Farenheit=Farenheit;
+Scanner scan= new Scanner(System.in);.
+System.out.print("Enter first number- ");
+double Farenheit= sc.nextInt();
+double Celsius=(32*Farenheit − 32) × 5/9
+System.out.println(Celsius is: "+Celsius);
+}
+}
