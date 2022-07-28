5d5a9e33edfc36d28f6db843cc01573c44ec4f74
Hsm3084 <94923387+Hsm3084@users.noreply.github.com>
2022-07-28 10:55:29 -0500

    Create Length.java

---
+++ Length.java
@@ -0,0 +1,29 @@
+int a = inch
+int b = foot
+int c = yard
+int d = mile
+int b = int a*12
+int c = int b*3
+int d = int c*1760
+char a = inch
+char b = foot
+char c = yard
+char d = mile
+Scanner scan= new Scanner(System.in);.
+System.out.print("Enter first number- ");
+int a= sc.nextInt();
+System.out.println("+inch");
+System.out.print("Enter first number- ");
+int b= sc.nextInt();
+System.out.println("+foot");
+System.out.print("Enter first number- ");
+int c= sc.nextInt();
+System.out.println("+yard");
+System.out.print("Enter first number- ");
+int d= sc.nextInt();
+System.out.println("+mile");
+Scanner scan= new Scanner(System.in);
+System.out.print(Enter char:);
+Scan.nextChar();
+System.out.print(Enter char:);
+Scan.nextChar();
